0a54cb88b231f1d8a1ccf35500b4a0a8ffbe5365
Aalzate95 <alex.alzate95@gmail.com>
2021-05-30 13:06:34 -0500

    tipo de recurso

--- src/views/resources/Resources.js
+++ src/views/resources/Resources.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import './Resources.css'
 
+const data = {
+    results:{
+        papers:[
+            {},
+            {},
+            {},
+        ],
+        tutorials:[
+
+        ]
+    }
+}
+
+
+const renderResourcesCards = Object.keys(data.results).map((resourceKind)=>{
+        let totalElements = data.results[resourceKind].length;
+        return(
+            <div>
+                <div>
+                    <h3>{resourceKind}</h3>
+                    <p>Cantidad de elementos: {totalElements}</p>
+                </div>
+            </div>
+            )
+    })
+
+    
+
+
 const Resources = () => {
     return ( 
-    <div>
-        Recursos
+    <div className="Resources">
+        <div>
+            {renderResourcesCards}
+        </div>
     </div> 
     );
 }
