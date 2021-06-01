7110e1873f9b4df20bf3220cb927aa64539ae721
Aalzate95 <alex.alzate95@gmail.com>
2021-06-01 02:06:12 -0500

    Limpieza ejemplo evaluaciones y corrección estilo

--- src/views/evaluation/Evaluation.js
+++ src/views/evaluation/Evaluation.js
@@ -1,51 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 
 const Evaluation = () => {
-    const [texto,setText] = useState('')
-    const [nombre, setNombre] = useState(null)
-
-    const OnClickEnviar = ()=>{
-        alert(
-            `El usuario ${nombre}, envió: ${texto}`
-        )
-    }
-
-    const listaDatos = [
-        {nombre:"recurso1", descripcion:"lonadsjdansda", calificaciones:[12,34]},
-        {nombre:"recurso3", descripcion:"dasdjnasdaknd", calificaciones:[13,43]}
-    ]
-
-    const renderDatos = listaDatos.map((e)=>{
-        return(
-            <div>
-                <h1>{e.nombre}</h1>
-                <p>{e.descripcion}</p>
-                {e.calificaciones.map((calificacion)=>{
-                    return(
-                        <h4>{calificacion}</h4>
-                        )
-                })}
-            </div>
-            )
-    })
-
+    
     return ( 
         <div>
-            <label>
-            Nombre:
-                <input type="text" onChange={(e)=>{setNombre(e.target.value)}}/>
-            </label>
-            <label>
-                Mensaje:
-                <input type="text" onChange={(e)=>{setText(e.target.value)}}/>
-            </label>
-            
-            <br/>
-            <button onClick={()=>{OnClickEnviar()}}>Enviar</button>
-
-            <br/>
-            {renderDatos}
+            Evaluation
         </div>
      );
 }
--- src/views/resources/Resources.js
+++ src/views/resources/Resources.js
@@ -3,6 +3,12 @@ import './Resources.css'
 import { useHistory } from 'react-router-dom';
 import data from '../../data/dataResources.json'
 
+const colors = [
+    "rgb(255, 98, 98)",
+    "rgb(255, 200, 98)",
+    "rgb(98, 255, 98)"
+]
+
 const Resources = () => {
 
     const history = useHistory();
@@ -10,7 +16,7 @@ const Resources = () => {
     const renderResourcesCards = Object.keys(data.results).map((resourceKind,index)=>{
         let totalElements = data.results[resourceKind].length;
         return(
-            <div className="card" key={index} onClick={()=>history.push(`/resources/${resourceKind}`)}>
+            <div className="card" key={index} style={{backgroundColor:`${colors[index]}`}} onClick={()=>history.push(`/resources/${resourceKind}`)}>
                 <div>
                     <h3>{resourceKind}</h3>
                     <p>Cantidad de elementos: {totalElements}</p>
