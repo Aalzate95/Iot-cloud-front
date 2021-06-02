dab78b7ce1c5cf6d5137f44ccf999ac7e473885c
Jaime Alarcón <jaiaalar@espol.edu.ec>
2021-06-02 04:40:18 -0500

    FORO VIEW

--- src/views/Foro/Foro.js
+++ src/views/Foro/Foro.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Foro = () => {
-    return ( 
-        <div>
-            Realizar Pregunta 
-        </div>
-     );
-}
- 
-export default FeedBack;
+  const [question,setQuestion] = useState('')
+  const [tema, setTema] = useState(null)
+
+  const OnClickEnviar = ()=>{
+      alert(
+          `Se envió la siguiente pregunta: ${question}`
+      )
+  }
+
+  const listaDatos = [
+      {nombre:"recurso1", descripcion:"lonadsjdansda", calificaciones:[12,34]},
+      {nombre:"recurso3", descripcion:"dasdjnasdaknd", calificaciones:[13,43]}
+  ]
+
+  const renderDatos = listaDatos.map((e)=>{
+      return(
+          <div>
+              <h1>{e.nombre}</h1>
+              <p>{e.descripcion}</p>
+              {e.calificaciones.map((calificacion)=>{
+                  return(
+                      <h4>{calificacion}</h4>
+                      )
+              })}
+          </div>
+          )
+  })
+
+  return ( 
+      <div>
+          <label>
+          Tema:
+              <select value={this.state.value} onChange={(e)=>{setTema(e.target.value)}}>
+              <option value="Tema1">Tema1</option>
+              <option value="Tema2">Tema2</option>
+              <option value="Tema3">Tema3</option>
+              <option value="Tema4">Tema4</option>
+              </select>
+          </label>
+          <label>
+              Mensaje:
+              <textarea type="text" onChange={(e)=>{setQuestion(e.target.value)}}/>
+          </label>
+          
+          <br/>
+          <button onClick={()=>{OnClickEnviar()}}>Enviar</button>
+
+          <br/>
+      </div>
+   );
+} 
+export default Foro;
